243e9d47bf054cfdfb24ba0b75b89279899099e0
doha belloute <yurimaichel@gmail.com>
2026-02-16 01:11:59 +0000

    project-setup + layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel CRUD Homework</title>
+</head>
+<body>
+
+<h2>Products CRUD</h2>
+<hr>
+
+@yield('content')
+
+</body>
+</html>
