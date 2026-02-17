c6250444f4dc9aebb859b12d6ccae23672437c87
doha belloute <yurimaichel@gmail.com>
2026-02-17 00:28:20 +0000

    ajout de migration de client

--- database/migrations/2026_02_16_221834_create_clients_table.php
+++ database/migrations/2026_02_16_221834_create_clients_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('clients', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+ public function up(): void
+{
+    Schema::create('clients', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email');
+        $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('clients');
