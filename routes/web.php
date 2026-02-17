02c95f5649dac7523aa6fbd33e39fdb9c26faced
doha belloute <yurimaichel@gmail.com>
2026-02-17 00:29:24 +0000

    route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ClientController;
 
-Route::resource('products', ProductController::class);
+Route::resource('clients', ClientController::class);
+// use App\Http\Controllers\ProductController;
+
+// Route::resource('products', ProductController::class);
 // Route::get('/', function () {
 //     return view('welcome');
 // });
